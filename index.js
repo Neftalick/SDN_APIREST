44d76646bed486c433acda89b32089c1151d441e
Ronny Pastor <a20185534@pucp.edu.pe>
2022-11-18 10:33:56 -0500

    fx

--- index.js
+++ index.js
@@ -119,16 +119,6 @@ app.post("/",(req,res)=>{
                 
                 break
             //Pensaba que el administrador mande sus credenciales cada vez que desee 
-            case "add service":
-                if(req.body.servicio != null){
-
-                }else{
-                    res.json({
-                        "status":"error",
-                        "error":"debe ingresar el servicio que desea agregar"
-                    })
-                }
-                break
         }
     }else{
         res.json({
