c6c77de5f6e55716a4ae64fbfec259bb5a98e706
JexUgaz <a20191417@pucp.edu.pe>
2022-11-18 10:58:45 -0500

    Formateado un poquito

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser")
 const pool = require("./pools")
 const e = require('express')
 const SHA512 = require("js-sha512").sha512
+
 //Obtenemos constante a partir de variables de entorno para aumentar la seguridad del servicio y realizamos las congifuraciones correspondientes
 dotenv.config()
 const app = express()
@@ -13,14 +14,20 @@ app.set('trust proxy', true)
 const pool_para_autenticar = pool.pool1
 const pool_usuarios_autenticados = pool.pool2
 const pool_servicios = pool.pool3
+
 //Solo expondremos un endpoint
 app.post("/",(req,res)=>{
     if(req.body.action != null){
         switch(req.body.action){
             case "status":
                 if(req.body.ip != null && req.body.mac != null){
-                    //Aqui se hace la consulta a la base de datos 
-                    pool_usuarios_autenticados.query(`SELECT u.idUsuario_Autenticado, u.Dispositivo_dispositivo_MAC, u.IP, r.nameROL FROM usuarios_autenticados.usuario_autenticado u INNER JOIN usuarios_autenticados.rol r ON u.Rol_idROL = r.idROL  WHERE u.IP = "${String(req.body.ip)}" AND u.Dispositivo_dispositivo_MAC = "${req.body.mac}"`,(err,results,fields)=>{
+
+                    //Aqui se hace la consulta a la base de datos
+                    pool_usuarios_autenticados.query(`SELECT u.idUsuario_Autenticado, u.Dispositivo_dispositivo_MAC, u.IP, r.nameROL FROM usuarios_autenticados.usuario_autenticado u 
+                                                    INNER JOIN usuarios_autenticados.rol r ON u.Rol_idROL = r.idROL  
+                                                    WHERE u.IP = "${String(req.body.ip)}" AND u.Dispositivo_dispositivo_MAC = "${req.body.mac}"`,
+                    (err,results,fields)=>{
+
                         if(err == null){
                             console.log(results)
                             if(results.length != 0){
@@ -42,7 +49,7 @@ app.post("/",(req,res)=>{
                                 "status":"error",
                                 "error":err
                             })
-                        }  
+                        }
                     })
                 }else{
                     res.json({
@@ -54,19 +61,25 @@ app.post("/",(req,res)=>{
                 break
             case "sign in":
                 if(req.body.user != null && req.body.password != null && req.body.mac != null && req.body.ip != null){
+
                     //Aqui se hace la consulta para autenticación
                     pool_para_autenticar.query(`SELECT * FROM usuarios_para_autenticar.usuario u
-                    inner join usuarios_para_autenticar.rol r on r.idROL = u.Rol_idRol
-                    inner join usuarios_para_autenticar.facultad f on f.idFacultad = u.Facultad_idFacultad
-                    where u.usuario = "${req.body.user}" and u.enable = 1;`,(err,result,fields)=>{
+                                                inner join usuarios_para_autenticar.rol r on r.idROL = u.Rol_idRol
+                                                inner join usuarios_para_autenticar.facultad f on f.idFacultad = u.Facultad_idFacultad
+                                                where u.usuario = "${req.body.user}" and u.enable = 1;`,
+                    (err,result,fields)=>{
                         if(err == null){
                             //password de prueba => password123
                             if(result.length != 0){
                                 if(SHA512(req.body.password) == result[0]["password"]){
                                     //debemos agregar manera para obtener la mac del equipo
-                                    pool_usuarios_autenticados.query(`INSERT INTO usuarios_autenticados.dispositivo (dispositivo_ID, dispositivo_MAC, name) VALUES ("${req.body.mac}", "${req.body.mac}", 'computador')`,(err2,result2,fields2)=>{
+                                    pool_usuarios_autenticados.query(`INSERT INTO usuarios_autenticados.dispositivo (dispositivo_ID, dispositivo_MAC, name) 
+                                                                    VALUES ("${req.body.mac}", "${req.body.mac}", 'computador')`,
+                                    (err2,result2,fields2)=>{
                                         if(err2 == null){
-                                            pool_usuarios_autenticados.query(`INSERT INTO usuarios_autenticados.usuario_autenticado (idUsuario_Autenticado,Dispositivo_dispositivo_MAC,diferenciador,switch_MAC,IP,Facultad_facultad_ID,Rol_idROL) VALUES ("${req.body.user}","${req.body.mac}","0","00:00:00:00:01:01","${req.body.ip}","${result[0]["Facultad_idFacultad"]}","${result[0]["Rol_idRol"]}")`,(err3,results3,fields3)=>{
+                                            pool_usuarios_autenticados.query(`INSERT INTO usuarios_autenticados.usuario_autenticado (idUsuario_Autenticado,Dispositivo_dispositivo_MAC,diferenciador,switch_MAC,IP,Facultad_facultad_ID,Rol_idROL) 
+                                                                            VALUES ("${req.body.user}","${req.body.mac}","0","00:00:00:00:01:01","${req.body.ip}","${result[0]["Facultad_idFacultad"]}","${result[0]["Rol_idRol"]}")`,
+                                            (err3,results3,fields3)=>{
                                                 if(err3 == null){
                                                     res.json({
                                                         "status":"OK"
